29ef1fa541ce2712f9fc519a881e06190ccbd34a
anu <anu_a4@hotmail.com>
2012-09-21 21:39:15 +0100

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+  @message = "hey, wassup?"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinsteallerDemo::Application.config.secret_token = '80f5f70cf4c4e0e17a7cbaea2420aef2ff2d49c9a2cbdc75e2ca62b748801dbc0743a847fd69417bf828bd10007bdca4d0858b8547e2c3f2feea0ef1d967ad24'
